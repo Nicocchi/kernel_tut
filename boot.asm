6ffeac76d36f8f21ccb0c37d9410a737a90bce21
Nicocchi <nicocchilover@gmail.com>
2025-05-11 13:23:33 -0400

    switched to 32bit protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7C00
 BITS 16     ; Use 16bit architecture. Will make sure that the assembler assembles instructions in 16bit code
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -10,85 +13,76 @@ times 33 db 0   ; Creates 33 bytes after the short jump
 
 
 start:
-    jmp 0x7C0:step2 ; Specifies the segment 0x7C0 so that the code segment register gets replaced by 0x7C0
-
-; DISK - READ SECTOR(S) INTO MEMORY
-; https://www.ctyme.com/intr/rb-0607.htm
-; AH = 02h
-; AL = number of sectors to read (must be nonzero)
-; CH = low eight bits of cylinder number
-; CL = sector number 1-63 (bits 0-5)
-; high two bits of cylinder (bits 6-7, hard disk only)
-; DH = head number
-; DL = drive number (bit 7 set for hard disk) - Already set for us by the BIOS
-; ES:BX -> data buffer
-
-; Return:
-; CF set on error
-; if AH = 11h (corrected ECC error), AL = burst length
-; CF clear if successful
-; AH = status (see #00234)
-; AL = number of sectors transferred (only valid if CF set for some
-; BIOSes)
-
+    jmp 0:step2
 
 ; Prints the character 'A' to the screen
 step2:
     cli                 ; Clear interrupts
     
     ; Segment registers
-    ; Since we are setting origin to 0, we are moving the segment registers ourselves to set to 0x7C00
-    ; If we don't set these ourselves, and relied on the bios to set them for us, it might set them to
-    ; 0x7C0 or might set it to 0 and assume the origin is 0x7C00. We don't know what the registers will be
-    ; when we load them.
-    mov ax, 0x7C0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
 
     ; Stack segment
-    mov ax, 0x00
     mov ss, ax          ; Set the stack segment now to 0
     mov sp, 0x7C00      ; Stack pointer
     sti                 ; Enable interrupts
 
-    mov ah, 2   ; Read sector command
-    mov al, 1   ; One sector to read
-    mov ch, 0   ; Cylinder low eight bits
-    mov cl, 2   ; Read sector 2
-    mov dh, 0   ; Head number
-    mov bx, buffer
-    int 0x13    ; Invoke the read command
-    jc error    ; If carry flag is set, jump to error
+.load_protected:
+    cli
+    ; It will go to the gdt_descriptor, find the size and the offset, look into there and load the gdt_code & gdt_data
+    lgdt[gdt_descriptor]    ; Loads the GDT
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32     ; Switches to the code selector and loads load32 - CODE_SEG will be replaced by 0x8
 
-    mov si, buffer
-    call print
+; GDT - Global Descriptor Table
+; https://wiki.osdev.org/Global_Descriptor_Table
+; These values will be defaults as a paging model is planned to be used. Unlocks all the memory
+gdt_start:
+gdt_null:   ; Just a null descriptor of 64 bits
+    dd 0x0
+    dd 0x0
+
+; Offset 0x8 - Where we are about to start to make the code descriptor
+gdt_code:           ; CS Should point to this
+    dw 0xFFFF       ; Segment limit first 0-15 bits
+    dw 0            ; Base first 015 bits
+    db 0            ; Base 16-23 bits
+    db 0x9A         ; Access byte
+    db 11001111b    ; High 4 bit flags and low 4 bit flags
+    db 0            ; Base 24-31 bits
+
+; Offset 0x10 - For the data segment, stack segment and so on
+gdt_data:           ; DS, SS, ES, FS, GS
+    dw 0xFFFF       ; Segment limit first 0-15 bits
+    dw 0            ; Base first 015 bits
+    db 0            ; Base 16-23 bits
+    db 0x92         ; Access byte
+    db 11001111b    ; High 4 bit flags and low 4 bit flags
+    db 0            ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  ; Gives the size of the descriptor
+    dd gdt_start                ; Offset
+
+[BITS 32]
+load32:
+    ; Load data registers
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000     ; Set the base pointer to point to 0x00200000
+    mov esp, ebp            ; Set the stack pointer to the base pointer - Setting further into memory
 
     jmp $
 
-error:
-    mov si, error_message
-    call print
-    jmp $               ; Keep jumping to itself
-
-print:
-    mov bx, 0
-.loop:
-    lodsb       ; Load the character that the si register is pointing to and load it to the al register and increment the si register
-    cmp al, 0   ; Checks for null terminator
-    je .done    ; If equal to 0, jump to done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10    ; Calling the bios video interrupt - Teletype Output
-    ret
-
-error_message: db 'Failed to load sector', 0
-
 times 510-($ - $$)db 0  ; Fill at least 510 bytes of data
 dw 0xAA55   ; Intel machines are little indian so the bytes get flipped when working with words. So it becomes '55AA'
-
-buffer:
