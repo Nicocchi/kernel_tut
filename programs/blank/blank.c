793667411727bff695634322de5e335f9cf505cb
Nicocchi <nicocchilover@gmail.com>
2025-09-04 11:45:42 -0400

    string tokenizer and memcpy, memset, memcmp functions added to stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,10 +1,19 @@
 #include "pandora.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 int main(int argc, char** argv)
 {
-    printf("I really love Yazawa Nico! %i\n", 25252);
+    char words[] = "I really love Nico";
+    const char* token = strtok(words, " ");
+
+    while (token)
+    {
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
+    // printf("I really love Yazawa Nico! %i\n", 25252);
 
     while(1)
     {
---
+++ programs/stdlib/src/string.h
@@ -0,0 +1,20 @@
+#ifndef PANDORA_STRING_H
+#define PANDORA_STRING_H
+
+#include <stdbool.h>
+
+char tolower(char str1);
+int strlen(const char* ptr);
+int strnlen(const char* ptr, int max);
+// Read the string until it finds a null terminator or a terminator that is specified
+int strnlen_terminator(const char* str, int max, char terminator);
+int istrncmp(const char* str1, const char* str2, int n);
+int strncmp(const char* str1, const char* str2, int n);
+char* strcpy(char* dest, const char* src);
+char* strncpy(char* dest, const char* src, int count);
+bool isdigit(char c);
+int tonumericdigit(char c);
+char* strtok(char* str, const char* delimiters);
+
+
+#endif
